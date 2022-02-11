980a89c6d3b243fb0f783aa17e9ac30c3d4779be
Nikolai Arkharov <arharovoversteam@gmail.com>
2022-02-11 23:13:11 +0200

    add user logo, dark\light theme switcher

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 const themeSwitch = document.getElementById('theme-switcher');
 
 themeSwitch.addEventListener('change', () => {
-  document.body.classList.toggle('lightTheme');
+  if (document.body.classList.contains('darkTheme')) {
+    document.body.classList.replace('darkTheme', 'lightTheme');
+    document.getElementById("userLogo").src="img/userAvatar_lightTheme.svg";
+  } else {
+    document.body.classList.replace('lightTheme', 'darkTheme');
+    document.getElementById("userLogo").src="img/userAvatar_darkTheme.svg";
+  }
 });
