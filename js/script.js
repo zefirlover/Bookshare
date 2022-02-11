98214a68ce95bd49f6f4d4b0819a5ac7329bb03c
Nikolai Arkharov <arharovoversteam@gmail.com>
2022-02-11 23:16:06 +0200

    fix naming

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ const themeSwitch = document.getElementById('theme-switcher');
 themeSwitch.addEventListener('change', () => {
   if (document.body.classList.contains('darkTheme')) {
     document.body.classList.replace('darkTheme', 'lightTheme');
-    document.getElementById("userLogo").src="img/userAvatar_lightTheme.svg";
+    document.getElementById("userLogo").src = "img/userIconLT.svg";
   } else {
     document.body.classList.replace('lightTheme', 'darkTheme');
-    document.getElementById("userLogo").src="img/userAvatar_darkTheme.svg";
+    document.getElementById("userLogo").src = "img/userIconDT.svg";
   }
 });
