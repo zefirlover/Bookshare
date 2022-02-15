ccbbf9f4a8a56af90c888499f87db31871a000e7
zefirlover <ibodikmushta@gmail.com>
2022-02-15 16:30:29 +0200

    Add searchBlock

--- js/script.js
+++ js/script.js
@@ -4,8 +4,12 @@ themeSwitch.addEventListener('change', () => {
   if (document.body.classList.contains('darkTheme')) {
     document.body.classList.replace('darkTheme', 'lightTheme');
     document.getElementById("userLogo").src = "img/userIconLT.svg";
+    document.getElementById("searchIcon").src = "img/searchIconLT.svg";
+    document.getElementById("filterIcon").src = "img/filterIconLT.svg";
   } else {
     document.body.classList.replace('lightTheme', 'darkTheme');
     document.getElementById("userLogo").src = "img/userIconDT.svg";
+    document.getElementById("searchIcon").src = "img/searchIconDT.svg";
+    document.getElementById("filterIcon").src = "img/filterIconDT.svg";
   }
 });
