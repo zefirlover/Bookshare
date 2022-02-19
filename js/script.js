74ed4263614adfbba904cfe64aa06ca427129dc1
zefirlover <ibodikmushta@gmail.com>
2022-02-19 16:37:59 +0200

    Fix minor issues and start to make userPage

--- js/script.js
+++ js/script.js
@@ -4,20 +4,32 @@ themeSwitch.addEventListener('change', () => {
   if (document.getElementById('themeBody').classList.contains('darkTheme')) {
     document.getElementById('themeBody').classList.replace('darkTheme', 'lightTheme');
     document.getElementById("shoppingIcon").src = "img/icon/shoppingIconLT.svg";
-    document.getElementById("searchIcon").src = "img/icon/searchIconLT.svg";
-    document.getElementById("filterIcon").src = "img/icon/filterIconLT.svg";
     document.getElementById("githubIcon").src = "img/icon/githubIconLT.svg";
     // linkedinIcon1 == linkedinIcon2
     document.getElementById("linkedinIcon1").src = "img/icon/linkedinIconLT.svg";
     document.getElementById("linkedinIcon2").src = "img/icon/linkedinIconLT.svg";
+    if (document.getElementById("searchIcon")) {
+      document.getElementById("searchIcon").src = "img/icon/searchIconLT.svg";
+      document.getElementById("filterIcon").src = "img/icon/filterIconLT.svg";
+    }
+    if (document.getElementById("userIcon")) {
+      document.getElementById("userIcon").src = "img/icon/userIconLT.svg";
+      document.getElementById("shoppingIcon1").src = "img/icon/shoppingIconLT.svg";
+    }
   } else {
     document.getElementById('themeBody').classList.replace('lightTheme', 'darkTheme');
     document.getElementById("shoppingIcon").src = "img/icon/shoppingIconDT.svg";
-    document.getElementById("searchIcon").src = "img/icon/searchIconDT.svg";
-    document.getElementById("filterIcon").src = "img/icon/filterIconDT.svg";
     document.getElementById("githubIcon").src = "img/icon/githubIconDT.svg";
     document.getElementById("linkedinIcon1").src = "img/icon/linkedinIconDT.svg";
     document.getElementById("linkedinIcon2").src = "img/icon/linkedinIconDT.svg";
+    if (document.getElementById("searchIcon")) {
+      document.getElementById("searchIcon").src = "img/icon/searchIconDT.svg";
+      document.getElementById("filterIcon").src = "img/icon/filterIconDT.svg";
+    }
+    if (document.getElementById("userIcon")) {
+      document.getElementById("userIcon").src = "img/icon/userIconDT.svg";
+      document.getElementById("shoppingIcon1").src = "img/icon/shoppingIconDT.svg";
+    }
   }
 });
 
