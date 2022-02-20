e7e94d0f81b876d783d6fe0d1c704c127dcd6ac0
zefirlover <ibodikmushta@gmail.com>
2022-02-20 13:51:43 +0200

    Add redaction logic

--- js/script.js
+++ js/script.js
@@ -44,3 +44,77 @@ function openFilterForm() {
     document.getElementById("filterForm").style.display = "block";
   }
 }
+
+function redactShow() {
+  isDisplayBlock = document.getElementById("staticLogin").style.display;
+
+  if(isDisplayBlock==""){
+    document.getElementById("staticLogin").style.display = "none";
+    document.getElementById("redactLogin").style.display = "block";
+
+    document.getElementById("staticName").style.display = "none";
+    document.getElementById("redactName").style.display = "block";
+
+    document.getElementById("staticSurname").style.display = "none";
+    document.getElementById("redactSurname").style.display = "block";
+
+    document.getElementById("staticEmail").style.display = "none";
+    document.getElementById("redactEmail").style.display = "block";
+
+    document.getElementById("staticPhone").style.display = "none";
+    document.getElementById("redactPhone").style.display = "block";
+
+    document.getElementById("redactSave").style.display = "block";
+    if (document.getElementById('themeBody').classList.contains('darkTheme')) {
+      document.getElementById("redactIcon").src = "img/icon/crossIconDT.svg";
+    } else {
+      document.getElementById("redactIcon").src = "img/icon/crossIconLT.svg";
+    }
+  } else {
+    if(isDisplayBlock=="block"){
+      document.getElementById("staticLogin").style.display = "none";
+      document.getElementById("redactLogin").style.display = "block";
+  
+      document.getElementById("staticName").style.display = "none";
+      document.getElementById("redactName").style.display = "block";
+  
+      document.getElementById("staticSurname").style.display = "none";
+      document.getElementById("redactSurname").style.display = "block";
+  
+      document.getElementById("staticEmail").style.display = "none";
+      document.getElementById("redactEmail").style.display = "block";
+  
+      document.getElementById("staticPhone").style.display = "none";
+      document.getElementById("redactPhone").style.display = "block";
+
+      document.getElementById("redactSave").style.display = "block";
+      if (document.getElementById('themeBody').classList.contains('darkTheme')) {
+        document.getElementById("redactIcon").src = "img/icon/crossIconDT.svg";
+      } else {
+        document.getElementById("redactIcon").src = "img/icon/crossIconLT.svg";
+      }
+    } else {
+      document.getElementById("staticLogin").style.display = "block";
+      document.getElementById("redactLogin").style.display = "none";
+      
+      document.getElementById("staticName").style.display = "block";
+      document.getElementById("redactName").style.display = "none";
+  
+      document.getElementById("staticSurname").style.display = "block";
+      document.getElementById("redactSurname").style.display = "none";
+  
+      document.getElementById("staticEmail").style.display = "block";
+      document.getElementById("redactEmail").style.display = "none";
+  
+      document.getElementById("staticPhone").style.display = "block";
+      document.getElementById("redactPhone").style.display = "none";
+
+      document.getElementById("redactSave").style.display = "none";
+      if (document.getElementById('themeBody').classList.contains('darkTheme')) {
+        document.getElementById("redactIcon").src = "img/icon/pencilIconDT.svg";
+      } else {
+        document.getElementById("redactIcon").src = "img/icon/pencilIconLT.svg";
+      }
+    }
+  }
+}
